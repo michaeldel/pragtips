8d04ff36f278085443ca83d7fda322df58ca0b81
michaeldel <michaeldel@protonmail.com>
2021-02-23 07:28:55 +0100

    Add modern vim tips extraction

--- extract.sh
+++ extract.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 pdftk "$practicalvimpdf" dump_data_utf8 | grep 'BookmarkTitle: Tip' | sed -E 's/BookmarkTitle: Tip ([0-9]+)\. /Tip \1\t/' > out/practivalvim.txt
+pdftk "$modernvimpdf" dump_data_utf8 | grep 'BookmarkTitle: Tip' | sed -E 's/BookmarkTitle: Tip ([0-9]+)\. /Tip \1\t/' > out/modernvim.txt
 
